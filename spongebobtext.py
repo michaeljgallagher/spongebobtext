eca3f59f53818a1919c294af9062e07229b7d89e
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-05-30 18:01:56 -0400

    Output now copies to clipboard via pyperclip

--- spongebobtext.py
+++ spongebobtext.py
@@ -1,5 +1,8 @@
+import pyperclip
+
 def sponge(s):
     return ''.join(c.upper() if i % 2 == 1 else c for i, c in enumerate(s.lower()))
 
 sbt = sponge(input('Enter text:\n'))
 print(sbt)
+pyperclip.copy(sbt)
